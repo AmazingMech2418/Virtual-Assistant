c7c9dc56848f14509b5695bcf76f046842e2677f
amazinigmech2418 <33498997+amazinigmech2418@users.noreply.github.com>
2018-09-13 09:42:11 -0400

    Create scratchAPI.js

---
+++ scratchAPI.js
@@ -0,0 +1,18 @@
+addWord("apiMessages");
+addSynonym("apiMessages","messages of");
+addSynonym("apiMessages","notifications of");
+addKnowledge("apiMessages","getAPI",true);
+// Note: jQuery needed
+
+$.noConflict
+            function require(url) {
+                var rtrn
+             jQuery.ajax({async:false,url: url, success: function(result){
+                if(url.search(".json")!==-1) {rtrn= result;} else if(url.search(".js")!==-1) {rtrn= eval("function() {"+result+"}");} else {
+            rtrn= result;
+                }
+        }});return rtrn;
+            }
+function getAPI(e) {
+return require("https://api.scratch.mit.edu/users/"+e.split(" ")[e.split(" ").length-1]+"/messages/count");
+}
